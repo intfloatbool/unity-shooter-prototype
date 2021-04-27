64f633dab3ada8a9aa03a01d31e4f3a960ebae88
intfloatbool <intfloatbool@gmail.com>
2021-04-27 10:36:00 +0300

    Rotation copier

---
+++ Assets/_Scripts/Common/TransformRotationCopier.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace _Scripts.Common
+{
+    public class TransformRotationCopier : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+
+        private void Update()
+        {
+            if (_target.Equals(null))
+                return;
+
+            transform.rotation = _target.rotation;
+        }
+    }
+}
