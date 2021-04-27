553cf28b026947eed23cffb73964fc34ce53f64a
intfloatbool <intfloatbool@gmail.com>
2021-04-27 13:13:13 +0300

    Shot delay

--- Assets/_Scripts/Battle/Weapons/FirearmWeapon.cs
+++ Assets/_Scripts/Battle/Weapons/FirearmWeapon.cs
@@ -8,7 +8,15 @@ namespace _Scripts.Battle.Weapons
     public class FirearmWeapon : WeaponBase
     {
         [SerializeField] private WeaponParams _weaponParams;
+        
+        [Space]
+        [Header("Runtime")]
+        [SerializeField] private bool _isReadyToShot;
 
+
+        private float _reloadTimer;
+        private float _shotDelayTimer;
+        
         private void Awake()
         {
             Assert.IsNotNull(_weaponParams, "_weaponParams != null");
@@ -16,7 +24,36 @@ namespace _Scripts.Battle.Weapons
 
         protected override void OnShotStart()
         {
-            throw new NotImplementedException();
+            if (!_isReadyToShot)
+                return;
+            
+            Debug.Log($"{name} shot!");
+            _isReadyToShot = false;
+        }
+
+        private void Update()
+        {
+            HandleWeaponLoop();
+        }
+
+        private void HandleWeaponLoop()
+        {
+            HandleShotDelayTimerLoop();
+        }
+
+        private void HandleShotDelayTimerLoop()
+        {
+            if (_isReadyToShot)
+                return;
+
+            if (_shotDelayTimer >= _weaponParams.delay)
+            {
+                _isReadyToShot = true;
+                _shotDelayTimer = 0f;
+                return;
+            }
+
+            _shotDelayTimer += Time.deltaTime;
         }
     }
 }
